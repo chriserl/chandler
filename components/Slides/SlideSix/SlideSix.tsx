9c755256f2c3159d459c454b6d950150eccbcb68
chriserldev <chriserldev@gmail.com>
2022-04-15 15:27:38 +0000

    revision: converted Quote component to SlideSix

--- components/Slides/SlideSix/SlideSix.tsx
+++ components/Slides/SlideSix/SlideSix.tsx
@@ -1,19 +1,19 @@
 import type { FC } from "react";
 import GenericSlide from "../GenericSlide/GenericSlide";
 import { Icon } from "../../Interactive/Icon/Icon";
-import qt from "./quote.module.scss";
+import slide from "./slidesix.module.scss";
 
 const Quote: FC = () => {
 	return (
 		<GenericSlide shade="light">
-			<div className={qt.qt}>
+			<div className={slide.slide}>
 				<Icon name="bookmark" size="large" padding={false} solid={true} />
-				<p className={`heading-4 ${qt["title"]}`}>Verse of the day</p>
-				<div className={qt["quote"]}>
-					<p className={`title-1 ${qt["quoteText"]}`}>
+				<p className={`heading-4 ${slide["title"]}`}>Verse of the day</p>
+				<div className={slide["mainTextArea"]}>
+					<p className={`title-1 ${slide["mainText"]}`}>
 						"Blessed are they that mourn: for they shall be comforted"
 					</p>
-					<p className={`caption-2 ${qt["version"]}`}>
+					<p className={`caption-2 ${slide.details}`}>
 						Matthew 4:3 {`[King James Version]`}
 					</p>
 				</div>
