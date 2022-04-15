9014399f566c19b69ec31c456b396e18e61db8d9
chriserldev <chriserldev@gmail.com>
2022-04-15 16:24:11 +0000

    component: added color option to icon component

--- components/Interactive/Icon/Icon.tsx
+++ components/Interactive/Icon/Icon.tsx
@@ -2,22 +2,22 @@ import type { FC } from "react";
 import { Icon } from "../../../utils/interfaces/Components/Icon";
 import icon from "./icon.module.scss";
 
-const Icon: FC<Icon> = ({ name, padding, weight, size }) => {
+const Icon: FC<Icon> = ({ name, padding, weight, size, color }) => {
 	return (
 		<div className={icon.icon}>
 			{weight === "solid" && (
 				<i
 					className={`bx bxs-${name} ${icon[size + "Icon"]} ${
-						!padding && icon.unPad
-					}`}
+						color && icon[color]
+					} ${!padding && icon.unPad}`}
 				></i>
 			)}
 
 			{weight === "line" && (
 				<i
 					className={`bx bx-${name} ${icon[size + "Icon"]} ${
-						!padding && icon.unPad
-					}`}
+						color && icon[color]
+					} ${!padding && icon.unPad}`}
 				></i>
 			)}
 		</div>
--- utils/interfaces/Components/Icon.ts
+++ utils/interfaces/Components/Icon.ts
@@ -3,4 +3,5 @@ export interface Icon {
 	padding: false | true;
 	weight: "line" | "solid";
 	size: "small" | "regular" | "medium" | "large" | "xlarge" | "xxlarge";
+	color?: "primary" | "secondary" | "gray" | "black";
 }
