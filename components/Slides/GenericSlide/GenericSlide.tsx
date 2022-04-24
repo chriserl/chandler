6e38d81add4a4b388addf00a429cc55c89fea0a6
chriserldev <chriserldev@gmail.com>
2022-04-24 01:46:57 +0000

    bugFix: fixed type bug on GenericSlide component

--- components/Slides/GenericSlide/GenericSlide.tsx
+++ components/Slides/GenericSlide/GenericSlide.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 import { GenericSlideData } from "../../../utils/interfaces/Components/Slides";
 import gs from "./genericslide.module.scss";
 
-const GenericSlide: FC<GenericSlide> = ({ shade, children }) => {
+const GenericSlide: FC<GenericSlideData> = ({ shade, children }) => {
 	return <div className={gs[shade]}>{children}</div>;
 };
 
