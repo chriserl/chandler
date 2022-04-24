8f70c779bffe71f9a018af7fcc592c51d9a11afb
chriserldev <chriserldev@gmail.com>
2022-04-24 01:50:45 +0000

    bugFix: fixed http-equiv meta prop in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,6 @@ const Home: NextPage = () => {
 		<div className={index.index}>
 			<Head>
 				<title>Youth Network Global</title>
-				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
 			<main>
